cb8e0a199feda26f29444999e164b707a5ccc25c
Ilya Novak <rickmortyand4@gmail.com>
2020-12-15 10:34:49 +0300

    Add config for express and sequelize

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require('sequelize');
+
+const name = process.env.DB_NAME;
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+const host = process.env.DB_HOST;
+
+const sequelize = new Sequelize(name, user, pass, {
+    host: host,
+    dialect: 'mysql',
+});
+
+try {
+    sequelize.authenticate();
+} catch(err) {
+    console.error(err);
+}
+
+module.exports = {
+    sequelize
+};
