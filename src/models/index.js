db10b06af28e7de959a5b4bbe375913cbbcc8f1f
Ilya Novak <rickmortyand4@gmail.com>
2020-12-17 01:02:23 +0300

    Add to model/index.js export for PostApu

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@ const { sequelize } = require('../config/db');
 
 const { RoleApi } = require('./role/index');
 const { UserApi } = require('./user/index');
+const { PostApi } = require('./post/index');
 
 const sync = async () => {
     await sequelize.sync({ alter: true });
@@ -10,5 +11,6 @@ const sync = async () => {
 module.exports = {
     RoleApi,
     UserApi,
+    PostApi,
     sync,
 };
