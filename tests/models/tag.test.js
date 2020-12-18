4e9d15ef04d19c939bb2e2f91ff875fb4bfec701
Ilya Novak <rickmortyand4@gmail.com>
2020-12-18 23:52:41 +0300

    Added tests for tag api

---
+++ tests/models/tag.test.js
@@ -0,0 +1,59 @@
+const assert = require('assert');
+const { TagApi, PostApi, UserApi, RoleApi } = require('../../src/models/index');
+
+describe('Test for tag api', async function() {
+    const tag = {
+        title: 'React',
+    };
+    let roleId = 0;
+
+    it ('Should create tag', async function() {
+        const role = await RoleApi.create('User');
+
+        const user = await UserApi.create({
+            firstName: 'Ilya',
+            lastName: 'Novak',
+            username: 'ilyaNovak',
+            email: 'rickmortyand4@gmail.com',
+            bio: '',
+            password: '123456',
+            salt: '',
+            roleId: role.id,
+        });
+        roleId = role.id;
+
+        const post = await PostApi.create({
+            title: 'My first project',
+            description: 'I tell you about problems with wich i will face',
+            preview: 'preview.png',
+            text: 'bla bla bla',
+            visible: false,
+            userId: user.id,
+        });
+        
+        const newTag = await TagApi.create(tag);
+        tag.id = newTag.id;
+        tag.postId = newTag.postId;
+
+        assert.deepStrictEqual(newTag, tag);
+    });
+
+    it ('Should get tag by id', async function() {
+        const tagData = await TagApi.getById(tag.id);
+        assert.deepStrictEqual(tagData, tag);
+    });
+
+    it ('Should get tags by post id', async function() {
+        const tags = await TagApi.getById(tag.postId);
+        assert.deepStrictEqual(tags, [tag]);
+    });
+
+    it ('Should delete by id', async function() {
+        await TagApi.deleteById(tag.id);
+        const tagData = await TagApi.getById(tag.id);
+
+        await RoleApi.deleteById(roleId);
+        assert.deepStrictEqual(tagData, {});
+    });
+
+});
