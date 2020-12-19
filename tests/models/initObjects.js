09d428ee43c78d4b4401d332d39c6a41ab541731
Ilya Novak <rickmortyand4@gmail.com>
2020-12-20 00:02:54 +0300

    took out the initialization objects

---
+++ tests/models/initObjects.js
@@ -0,0 +1,46 @@
+const user = {
+    firstName: 'Ilya',
+    lastName: 'Novak',
+    username: 'ilyaNovak',
+    email: 'rickmortyand4@gmail.com',
+    password: '123456',
+    salt: '',
+    roleId: -1,
+};
+
+const tag = {
+    title: 'React',
+};
+
+const project = {
+    preview: 'project_preview.png',
+    title: 'Chat',
+    description: 'Chat for people',
+    projectLink: 'https://chat.com',
+    githubLink: 'https://github.com',
+};
+
+const post = {
+    title: 'My first project',
+    description: 'I tell you about problems with wich i will face',
+    preview: 'preview.png',
+    text: 'bla bla bla',
+    visible: false,
+};
+
+const file = {
+    name: 'main.png',
+};
+
+const comment = {
+    text: 'Cool post',
+};
+
+module.exports = {
+    user,
+    tag,
+    project,
+    post,
+    file,
+    comment,
+};
