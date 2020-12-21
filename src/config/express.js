91c8e84e33ebc1fe7ff20233fd67fee7c8b98c3f
Ilya Novak <rickmortyand4@gmail.com>
2020-12-21 23:20:59 +0300

    Added body parser and exress to export

--- src/config/express.js
+++ src/config/express.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
+app.use(bodyParser.json());
 
 module.exports = {
     app,
+    express, 
     port: process.env.PORT
 }
