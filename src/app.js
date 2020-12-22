5b11ee11421364deb7484982b4552a47c20b466b
Ilya Novak <rickmortyand4@gmail.com>
2020-12-23 01:19:00 +0300

    Added like api to app

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ app.use('/user', routes.UserRouter);
 app.use('/post', routes.PostRouter);
 app.use('/project', routes.ProjectRouter);
 app.use('/comment', routes.CommnetRouter);
-app.use('/file',routes.FileRouter);
+app.use('/file', routes.FileRouter);
+app.use('/like', routes.LikeRouter);
 
 module.exports = app;
