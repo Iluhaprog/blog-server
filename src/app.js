15e65204c73b99af12570e4e166888a90e74c549
Ilya Novak <rickmortyand4@gmail.com>
2020-12-22 22:29:03 +0300

    Added project routes to app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const { app } = require('./config/express');
-const { UserRouter, RoleRouter, PostRouter } = require('./routes');
+const { UserRouter, RoleRouter, PostRouter, ProjectRouter } = require('./routes');
 
 app.use('/role', RoleRouter);
 app.use('/user', UserRouter);
 app.use('/post', PostRouter);
+app.use('/project', ProjectRouter);
 
 module.exports = app;
