839088a885c83d4010eed906c5ccd0b95ce4f93b
Ilya Novak <rickmortyand4@gmail.com>
2020-12-22 14:14:10 +0300

    Added router to app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const { app } = require('./config/express');
-const { UserRouter, RoleRouter } = require('./routes');
+const { UserRouter, RoleRouter, PostRouter } = require('./routes');
 
 app.use('/role', RoleRouter);
 app.use('/user', UserRouter);
+app.use('/post', PostRouter);
 
 module.exports = app;
