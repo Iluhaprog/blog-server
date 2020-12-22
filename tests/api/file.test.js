5dea44550a424aded3541ee4c3b9589698275adf
Ilya Novak <rickmortyand4@gmail.com>
2020-12-23 00:52:14 +0300

    Added tests for file api

---
+++ tests/api/file.test.js
@@ -0,0 +1,62 @@
+const request = require('supertest');
+const assert = require('assert');
+const app = require('../../src/app');
+const { RoleApi, UserApi, PostApi } = require('../../src/models');
+const { userData, postData, fileData } = require('./initObjects');
+
+describe('Test for comment api of app', async function() {
+    let roleId = 0;
+
+    it('Should create file', async function() {
+        const role = await RoleApi.create('ROLE');
+        roleId = role.id;
+        userData.roleId = roleId;
+
+        const user = await UserApi.create(userData);
+        postData.userId = user.id;
+
+        const post = await PostApi.create(postData);
+        fileData.postId = post.id;
+
+        const { body } = await request(app)
+                            .post('/file/create')
+                            .set('Accept', 'application/json')
+                            .send({ file: fileData });
+        fileData.id = body.id;
+        fileData.date = body.date;
+        assert.deepStrictEqual(body, fileData);
+    });
+
+    it('Should get file by id', async function() {
+        const { body } = await request(app)
+                            .get(`/file/getById?id=${fileData.id}`)
+                            .send();
+        fileData.date = body.date;
+        assert.deepStrictEqual(body, fileData);
+    });
+
+    it('Should get files by post id', async function() {
+        const { body } = await request(app)
+                            .get(`/file/getByPostId?postId=${fileData.postId}`)
+                            .send();
+        assert.deepStrictEqual(body, [fileData]);
+    });
+
+    it('Should update file', async function() {
+        fileData.name = 'newName.png';
+        const { body } = await request(app)
+                                .put('/file/update')
+                                .set('Accept', 'application/json')
+                                .send({ file: fileData });
+        assert.deepStrictEqual(body, fileData);
+    });
+
+    it('Should delete file by id', async function() {
+        const res = await request(app)
+                        .delete(`/file/deleteById?id=${fileData.id}`)
+                        .send();
+        await RoleApi.deleteById(roleId);
+        assert.strictEqual(res.status, 204);
+    });
+
+});
