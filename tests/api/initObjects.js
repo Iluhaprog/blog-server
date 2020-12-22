86d0bf018d2d6793b13bd2a821a6392b218f07b4
Ilya Novak <rickmortyand4@gmail.com>
2020-12-22 14:08:10 +0300

    Put init objects in other file

---
+++ tests/api/initObjects.js
@@ -0,0 +1,24 @@
+const user = {
+    avatarImage: '',
+    firstName: 'Ilya',
+    lastName: 'Novak',
+    username: 'ilkass',
+    bio: 'Bla bla bla!',
+    email: 'email@email.email',
+    password: '12345',
+    skills: 'All',
+    roleId: 0,
+};
+
+const post = {
+    title: 'My first project',
+    description: 'I tell you about problems with wich i will face',
+    preview: 'preview.png',
+    text: 'bla bla bla',
+    visible: false,
+};
+
+module.exports = {
+    userData: user,
+    postData: post,
+};
