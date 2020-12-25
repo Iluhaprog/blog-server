44de2008063c2bf88d35d54689bea85483734c2a
Ilya Novak <rickmortyand4@gmail.com>
2020-12-25 16:21:39 +0300

    Added tests for crypt lib

---
+++ tests/libs/crypt.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const crypt = require('../../src/libs/crypt');
+
+describe('Tests for crypt lib', function() {
+    const testString = 'test_string';
+    const invalidString = 'invalid_string';
+    
+    it('Should create hash', async function() {
+        const newHash = await crypt.getPasswordHash(testString);
+        assert.strictEqual(typeof newHash, 'string');
+    });
+    
+    it('Should compare password with hash', async function() {
+        const hash = await crypt.getPasswordHash(testString);
+
+        const isEqual = await crypt.compare(testString, hash);
+        assert.strictEqual(isEqual, true);
+    });
+
+    it('Should compare invalid password with hash', async function() {
+        const hash = await crypt.getPasswordHash(testString);
+
+        const isEqual = await crypt.compare(invalidString, hash);
+        assert.strictEqual(isEqual, false);
+    }); 
+});
