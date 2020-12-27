7587ab3223d20d6705c8e38b78cb729bc411bcd0
Ilya Novak <rickmortyand4@gmail.com>
2020-12-27 14:44:01 +0300

    Added config for redis store

--- src/config/express.js
+++ src/config/express.js
@@ -5,12 +5,30 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const passport = require('./passport');
 const session = require('express-session');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
+
 const app = express();
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
+
+redisClient.on('error', function (err) {
+    console.log('Could not establish a connection with redis. ' + err);
+});
+redisClient.on('connect', function (err) {
+    console.log('Connected to redis successfully');
+});
 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(session({
+    store: new RedisStore({ client: redisClient }),
     secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+    },
 }));
 
 app.use(passport.initialize());
