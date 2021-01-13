6b62865c093f97db63e9dc348fc32d1eed5fe97b
Ilya Novak <rickmortyand4@gmail.com>
2021-01-13 10:07:12 +0300

    Fix app start

--- src/start.js
+++ src/start.js
@@ -1,4 +1,5 @@
-const { app, port } = require('./config/express');
+const { port } = require('./config/express');
+const app = require('./app');
 
 app.listen(port, () => {
     console.log(`App listen on port ${port}`);
