e86dbe57546b459ffe3958fca6da367fcbf92029
Ilya Novak <rickmortyand4@gmail.com>
2021-01-13 10:27:39 +0300

    Getting id from session for update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -79,8 +79,9 @@ async function create(req, res) {
 async function update(req, res) {
     try {
         const { user } = req.body;
-        await UserApi.update(user);
-        const updatedUser = await UserApi.getById(user.id);
+        const userData = { ...user, id: req.user.id };
+        await UserApi.update(userData);
+        const updatedUser = await UserApi.getById(req.user.id);
         res.json(updatedUser);
     } catch (error) {
         console.log(error);
