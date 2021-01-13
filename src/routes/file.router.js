fba89c4c6c588d4f0e26dae14020f22b0924fdf0
Ilya Novak <rickmortyand4@gmail.com>
2021-01-13 13:04:37 +0300

    Added uploader to create route

--- src/routes/file.router.js
+++ src/routes/file.router.js
@@ -1,11 +1,12 @@
 const { express, passport } = require('../config/express');
 const { FileController } = require('../controllers');
+const { uploader } = require('../libs/files');
 const { auth, access } = require('../libs/filters');
 
 const router = express.Router();
 
 router
-    .post('/create', [auth.isAuthorized, access.isAdmin], FileController.create)
+    .post('/create', [auth.isAuthorized, access.isAdmin, uploader], FileController.create)
     .get('/getById', FileController.getById)
     .get('/getByPostId', FileController.getByPostId)
     .put('/update', [auth.isAuthorized, access.isAdmin], FileController.update)
