012e849d9817fccd1cdf2979d80a14ded5e4bff5
Ilya Novak <rickmortyand4@gmail.com>
2021-02-28 14:41:09 +0300

    Revert "Added sort DESC for get all post"
    This reverts commit f13b95866f4f995f064653695dacbf4f805abce9.

--- src/models/post/api.js
+++ src/models/post/api.js
@@ -19,9 +19,6 @@ async function getAll({offset, limit}) {
             include: [ Tag ],
             offset,
             limit,
-            order: [
-                ['id', 'DESC']
-            ]
         });
         return getModelsDataArray(posts);
     } catch (error) {
