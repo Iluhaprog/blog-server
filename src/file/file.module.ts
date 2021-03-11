a7d66aa22bf0541b721373300ce855a3ffa929f9
Ilya Novak <rickmortyand4@gmail.com>
2021-03-11 13:50:43 +0300

    Added config for multer to  controller

--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { File } from './file.entity';
 import { FileService } from './file.service';
 import { FileController } from './file.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([File])],
+  imports: [
+    TypeOrmModule.forFeature([File]),
+    MulterModule.register({
+      dest: './uploads/',
+    }),
+  ],
   providers: [FileService],
   controllers: [FileController],
 })
