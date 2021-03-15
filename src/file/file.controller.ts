d6cd5979aad0ad60af7b9f10919422f9dca43f2b
Ilya Novak <rickmortyand4@gmail.com>
2021-03-15 10:19:14 +0300

    Fix path to ApiFile decorator

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -25,7 +25,7 @@ import { File } from './file.entity';
 import { CreateFileDto } from './dto/create-file.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiFile } from 'src/decorators/api-file.decorator';
+import { ApiFile } from '../decorators/api-file.decorator';
 
 @ApiTags('file')
 @Controller('file')
